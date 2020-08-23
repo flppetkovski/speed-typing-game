eeb3b6b6b3bdba9afb40f3d3a0e603812dd38eb6
unknown <flp_petkovski@yahoo.com>
2020-08-23 19:15:07 +0200

    Speed Typing Game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
-function App() {
+export default function App() {
+  const [text, setText] = useState("");
+  const [time, setTime] = useState(10);
+  const [wordCount, setWordCount] = useState(0);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const inputRef = useRef(null);
+
+  function startGame() {
+    setTime(10);
+    setIsTimeRunning(true);
+    inputRef.current.focus();
+    setText("");
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    countWords(text);
+  }
+
+  useEffect(() => {
+    if (time && isTimeRunning > 0) {
+      setTimeout(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+    } else {
+      endGame();
+    }
+  }, [time, isTimeRunning]);
+
+  function countWords(text) {
+    const sanitizedWords = text.trim().split(" ");
+    const filteredWords = sanitizedWords.filter((word) => word !== "").length;
+    setWordCount(filteredWords);
+  }
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setText(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>How Fast Can You Type?</h1>
+      <textarea
+        disabled={!isTimeRunning}
+        ref={inputRef}
+        value={text}
+        onChange={handleChange}
+      />
+      <h3>Time remaining: {time}</h3>
+      <h2>Word Count: {wordCount}</h2>
+      <button disabled={isTimeRunning} onClick={startGame}>
+        Start Game
+      </button>
     </div>
   );
 }
-
-export default App;
