6b6e5b32daff581a0453c334c62d3c5a23ad6bc9
unknown <flp_petkovski@yahoo.com>
2020-08-23 19:29:56 +0200

    Speed Typing Game build

---
+++ build/precache-manifest.2e163a70691d7697f58dc031aed3e8d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef25be52683d0634d40ac052c75e226d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792832008769edf66508",
+    "url": "/static/js/2.1fafc278.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1fafc278.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aae09560b3e3e414e11",
+    "url": "/static/js/main.70b1953e.chunk.js"
+  },
+  {
+    "revision": "cc5083fbda640a9aa498",
+    "url": "/static/js/runtime-main.861822b6.js"
+  }
+]);
